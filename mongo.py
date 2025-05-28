8d81b471f58dfda8c0d55cc30850ffacc1c589b4
Rodrigo-SE03 <se03rodrigo@gmail.com>
2025-05-28 19:31:43 -0300

    .gitignore

--- mongo.py
+++ mongo.py
@@ -1,7 +1,10 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+user = os.getenv("MONGO_USER")
+password = os.getenv("MONGO_PASS")
 
-uri = "mongodb+srv://se03rodrigo:D7Os2Vm3h1TBhLMu@vectordatabase.ufiyeyf.mongodb.net/?retryWrites=true&w=majority&appName=VectorDatabase"
+uri = f"mongodb+srv://{user}:{password}@vectordatabase.ufiyeyf.mongodb.net/?retryWrites=true&w=majority&appName=VectorDatabase"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 def get_collection():
